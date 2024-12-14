d1414f8a12324abfde46d2f9208d69d8f02b096f
MinearK708 <129929585+MinearK708@users.noreply.github.com>
2024-12-14 12:49:30 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    for (let i = 0; i < 50; i++) {
+        const snowflake = document.createElement("span");
+        snowflake.textContent = "❄";
+        snowflake.className = "snowflake";
+        snowflake.style.left = `${Math.random() * 100}vw`;
+        snowflake.style.animationDelay = `${Math.random() * 10}s`;
+        snowflake.style.fontSize = `${Math.random() * 10 + 10}px`;
+        document.body.appendChild(snowflake);
+    }
+});
